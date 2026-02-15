755facf774ab839003a9b92c4f978dad4ef70a60
PreritRawat <rawatprerit1406@gmail.com>
2026-02-16 03:38:46 +0530

    chinchillastestpush
    hello world

---
+++ My project/Assets/Scripts/Movement system/chinchillastestscript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class chinchillastestscript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      Debug.Log("hello world"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+		
+
+}
